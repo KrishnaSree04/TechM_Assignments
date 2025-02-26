06f6b5008d046ca1930ea292eb960e7c0a757888
KrishnaSree04 <114429533+KrishnaSree04@users.noreply.github.com>
2025-02-26 19:58:45 +0530

    Add files via upload

---
+++ Daily_Tasks/26-02-25 TypeScript/Basics/task6.ts
@@ -0,0 +1,15 @@
+enum Color {
+    Red,
+    Green,
+    White,
+    Blue
+}
+let selectedColor: Color = Color.Green;
+console.log(`Selected color: ${selectedColor}`);
+
+// 10. Null and Undefined
+let isNull: null = null;
+console.log(isNull);
+
+let isUndefined: undefined = undefined;
+console.log(isUndefined);
---
+++ Daily_Tasks/26-02-25 TypeScript/Basics/task7.ts
@@ -0,0 +1,6 @@
+let number: number[] = [1, 2, 3, 4];
+numbers.push(5);
+console.log(number);
+numbers.pop();
+console.log(number);
+numbers.forEach(num => console.log(num));
